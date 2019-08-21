5159253fa735f5647e564b3a0ba9c64b919d0ede
Rich FitzJohn <rich.fitzjohn@gmail.com>
2019-08-21 10:39:46 +0100

    Add app that uses cookies for user detection

---
+++ inst/cookie/app.R
@@ -0,0 +1,62 @@
+devtools::load_all()
+
+ui <- fluidPage(
+  shiny::titlePanel("Example app"),
+  mod_cookies_ui("cookies"),
+  shiny::sidebarLayout(
+    shiny::sidebarPanel(
+      shiny::numericInput("a", "a", NULL),
+      shiny::numericInput("b", "b", NULL),
+      shiny::numericInput("c", "c", NULL),
+      shiny::actionButton("random", "Randomise",
+                          class = "btn-success",
+                          icon = shiny::icon("random"))),
+    shiny::mainPanel(
+      shiny::plotOutput("plot"))),
+  shiny::div(
+    shiny::hr(),
+    remotesave::mod_remotesave_ui("save")))
+
+
+server <- function(input, output, session) {
+  rv <- shiny::reactiveValues()
+  cookies <- shiny::callModule(mod_cookies_server, "cookies")
+
+  root <- shiny::reactive(TEST_ROOT)
+  user <- shiny::reactive(cookies$get("SRVID"))
+
+  get_state <- function() {
+    list(a = input$a, b = input$b, c = input$c)
+  }
+  set_state <- function(state) {
+    shiny::updateNumericInput(session, "a", value = state$a)
+    shiny::updateNumericInput(session, "b", value = state$b)
+    shiny::updateNumericInput(session, "c", value = state$c)
+  }
+
+  save <- shiny::callModule(
+    remotesave::mod_remotesave_server, "save",
+    root, user, get_state, set_state)
+
+  shiny::observeEvent(
+    input$random, {
+      shiny::updateNumericInput(session, "a", value = runif(1))
+      shiny::updateNumericInput(session, "b", value = runif(1))
+      shiny::updateNumericInput(session, "c", value = runif(1))
+    })
+
+  output$plot <- shiny::renderPlot({
+    x <- c(input$a, input$b, input$c)
+    if (any(is.finite(x))) {
+      tt <- seq(0, 4 * pi, length.out = 501)
+      yy <- outer(tt, x, function(t, x) sin(t / x))
+      matplot(tt, yy, type = "l", lty = 1, xlab = "time", ylab = "value")
+    }
+  })
+
+  list(get_state = get_state,
+       set_state = set_state)
+}
+
+## document.cookie="username=John Doe"; document.cookie="SRVID=foo";
+shiny::shinyApp(ui, server)
